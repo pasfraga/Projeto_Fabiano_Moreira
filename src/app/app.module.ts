636312f8d956fcf002be3a91db297da9282449f2
Técnico em Informática <24121742018.1@senacrjedu.local>
2019-09-16 19:21:36 -0300

    16092019

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,23 @@ import { AppComponent } from './app.component';
 import { PageNavComponent } from './pages/page-nav/page-nav.component';
 import { AddUsuarioComponent } from './pages/add-usuario/add-usuario.component';
 import { ListUsuarioComponent } from './pages/list-usuario/list-usuario.component';
+import { PageFooterComponent } from './pages/page-footer/page-footer.component';
+import { PageHeaderComponent } from './pages/page-header/page-header.component';
+import { PageHomeComponent } from './pages/page-home/page-home.component';
+import { ListProdutoComponent } from './pages/list-produto/list-produto.component';
+import { AddProdutoComponent } from './pages/add-produto/add-produto.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PageNavComponent,
     AddUsuarioComponent,
-    ListUsuarioComponent
+    ListUsuarioComponent,
+    PageFooterComponent,
+    PageHeaderComponent,
+    PageHomeComponent,
+    ListProdutoComponent,
+    AddProdutoComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/list-produto/list-produto.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-produto',
+  templateUrl: './list-produto.component.html',
+  styleUrls: ['./list-produto.component.css']
+})
+export class ListProdutoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
